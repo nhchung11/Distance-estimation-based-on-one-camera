1a1327bb556e2a720f64f45b23bf516dbc4794fa
nhchung11 <ngchung2002bn@gmail.com>
2024-06-03 01:06:00 +0700

    new method

--- utils/OpenCVServer.py
+++ utils/OpenCVServer.py
@@ -5,11 +5,11 @@ import numpy as np
 import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip = "192.168.0.103"
+ip = "192.168.0.102"
 # ip = "127.0.0.1"
 port = 6666
 s.bind((ip, port))
-counter = 34
+counter = 40
 
 while True:
     x = s.recvfrom(1000000)
@@ -26,7 +26,7 @@ while True:
     center = (w//2, h//2)
 
     # Draw the center point
-    # cv2.circle(img, center, radius=5, color=(0, 255, 0), thickness=-1)
+    cv2.circle(img, center, radius=5, color=(0, 255, 0), thickness=-1)
     # print(type(img))
     cv2.imshow('Img Server', img)
     if cv2.waitKey(1) & 0xFF == ord('s'):
